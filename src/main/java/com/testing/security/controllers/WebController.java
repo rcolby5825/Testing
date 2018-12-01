03011741ac3c139b2bd0282ee7d2a61e076cc718
rcolby5825 <aspen1331@yahoo.com>
2018-12-01 10:33:17 -0700

    file upload added

--- src/main/java/com/testing/security/controllers/WebController.java
+++ src/main/java/com/testing/security/controllers/WebController.java
@@ -1,4 +1,11 @@
 package com.testing.security.controllers;
 
+@Controller
 public class WebController {
+    @RequestMapping(value = "/fileUpload", method = RequestMethod.POST)
+    public String importParse(@RequestParam("myFile") MultipartFile myFile) {
+        // ... do whatever you want with 'myFile'
+        // Redirect to a successful upload page
+        return "redirect:uploadSuccess.html";
+    }
 }
