03d286219e95645f43e4bf4babfc3918c53df0b7
rcolby5825 <aspen1331@yahoo.com>
2018-12-01 11:52:08 -0700

    added upload success

--- src/main/java/com/testing/security/controllers/WebController.java
+++ src/main/java/com/testing/security/controllers/WebController.java
@@ -1,11 +1,21 @@
 package com.testing.security.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.ui.Model;
+
 @Controller
 public class WebController {
+    private static final String MODEL_SUCCESS = "success";
+    private static final String MODEL_ERROR = "error";
+
+
     @RequestMapping(value = "/fileUpload", method = RequestMethod.POST)
-    public String importParse(@RequestParam("myFile") MultipartFile myFile) {
-        // ... do whatever you want with 'myFile'
-        // Redirect to a successful upload page
-        return "redirect:uploadSuccess.html";
+    public String importParse(@RequestParam("myFile") MultipartFile myFile, Model model) {
+        model.addAttribute(MODEL_SUCCESS, "Successful loading of file.");
+        return "index.html";
     }
 }
